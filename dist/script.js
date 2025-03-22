f4aba1de0cb75b6546ee3e4364f3bbd1ab4724f7
Paulo de Araujo <127964717+Paulo-Araujo-Jr@users.noreply.github.com>
2025-03-22 08:57:12 -0300

    fix: Fixing JavaScript error

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+  var num1 = document.getElementById('num1').value;
+  var num2 = document.getElementById('num2').value;
+  
+  var resutl = parseInt(num1) + parseInt(num2);
+  
+  document.getElementById('result').innerHTML = "Resultado: " + result;
+}
